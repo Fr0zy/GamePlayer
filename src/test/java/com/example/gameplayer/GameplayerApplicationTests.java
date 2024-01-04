7bc905d67836fa0172f68e70a6a5d208fb883cf2
Fr0zy <kiril@zlotnyk.com>
2024-01-04 15:48:18 +0100

    First commit

---
+++ src/test/java/com/example/gameplayer/GameplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
