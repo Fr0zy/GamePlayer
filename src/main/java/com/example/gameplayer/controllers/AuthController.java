bb24d3748bf3e8ebacd2cd4710c28d8d2d45be02
Tymek <tsomov@edu.cdv.pl>
2024-01-12 23:39:29 +0100

    resolve FE problems

--- src/main/java/com/example/gameplayer/controllers/AuthController.java
+++ src/main/java/com/example/gameplayer/controllers/AuthController.java
@@ -17,7 +17,7 @@ public class AuthController {
     @GetMapping("/login")
     public String showLoginForm(Model model){
         model.addAttribute("loginForm", new UserLoginDTO());
-        return "loginForm";
+        return "LoginPage/SignIn/sign-in";
     }
 
     @PostMapping("/login")
